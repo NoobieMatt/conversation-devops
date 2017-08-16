9fb536b7b0926d844d6373d0eed536c829b6cf2b
Chris Desmarais <desmarchris@gmail.com>
2017-08-16 14:34:59 -0400

    added update_workspace

--- import-from-github.py
+++ import-from-github.py
@@ -3,6 +3,7 @@ import requests
 import base64
 
 # env variables
+prod_workspace_id = '' # only fill in if you already have a prod workspace
 prod_username = '' # conversation prod username
 prod_password = '' # conversation prod password
 github_username = ''
@@ -10,6 +11,7 @@ github_access_token = '' # personal access token for auth
 repo = ''
 github_endpoint = 'https://api.github.com/repos/%s/%s/contents/workspace.json' % (github_username, repo)
 conversation_url = 'https://gateway.watsonplatform.net/conversation/api/v1/workspaces?version=2017-05-26'
+update_conversation_url = 'https://gateway.watsonplatform.net/conversation/api/v1/workspaces/%s?version=2017-05-26' % (prod_workspace_id)
 
 # get json from github
 def get_file():
@@ -21,11 +23,18 @@ def get_file():
 
     return data
 
-new_workspace = get_file()
+workspace = get_file()
 
 # create new workspace in prod instance
 def create_workspace(workspace):
     response = requests.post(conversation_url, auth=(prod_username, prod_password), json=workspace)
     print response
 
-create_workspace(new_workspace)
+# update production workspace
+def update_workspace(workspace):
+    response = requests.post(update_conversation_url, auth=(prod_username, prod_password), json=workspace)
+    print response
+
+# run create or update
+create_workspace(workspace)
+# update_workspace(workspace)
